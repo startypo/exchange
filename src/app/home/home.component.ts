9ea821fed628dfed54b3033881b6ddc1bf873211
cnetopereira@hotmail.com <cnetopereira@hotmail.com>
2017-03-04 06:28:53 -0300

    Initial config

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  styleUrls: [ './home.component.css' ],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+
+}

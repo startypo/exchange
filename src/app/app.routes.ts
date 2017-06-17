7ba5ffea16adeb775ceb21186355739b41e59eec
Renan Garcia <renangarcia@live.com>
2017-06-17 03:56:43 -0300

    Continuação dos ajustes do layout da página de trocas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -39,6 +39,6 @@ const SECURE_ROUTES: Routes = [
 
 export const ROUTES: Routes = [
   { path: '', children: PUBLIC_ROUTES },
-  { path: '', children: SECURE_ROUTES, canActivate: [AuthGuard] },
+  { path: '', children: SECURE_ROUTES },
   { path: '**', component: NoContentComponent }
 ];
